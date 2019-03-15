fc603e537f2572e5366062e8c31f49681ad81092
jgalla <jessejgallagher@gmail.com>
2019-03-15 04:21:03 -0400

    Add filter in `Cocktails` handlechange method
    - Add `selectedIngredients` to state
    - Check intersection between `selectedIngredients` array and cocktails
      to be rendered in CardDeck. If length !== 0, then the current cocktail
      contains a selected ingredient and can be rendered.

--- src/cocktails/components/Cocktails.js
+++ src/cocktails/components/Cocktails.js
@@ -15,7 +15,8 @@ class Cocktails extends Component {
 
     this.state = {
       cocktails: null,
-      ingredients: null
+      ingredients: null,
+      selectedIngredients: []
     }
   }
 
@@ -57,7 +58,19 @@ class Cocktails extends Component {
     const index = event.target.getAttribute('index')
     const newIngredients = this.state.ingredients.slice()
     newIngredients[index].checked_status = !newIngredients[index].checked_status
-    this.setState({ ingredients: newIngredients })
+
+    const checked = event.target.checked
+    console.log(checked)
+    const ingredient = event.target.getAttribute('name')
+    const newSelectedIngredients = this.state.selectedIngredients.slice()
+    if (checked) {
+      newSelectedIngredients.push(ingredient)
+    } else {
+      const arrIndex = newSelectedIngredients.indexOf(ingredient)
+      newSelectedIngredients.splice(arrIndex, 1)
+    }
+
+    this.setState({ ingredients: newIngredients, selectedIngredients: newSelectedIngredients })
   }
 
   render () {
@@ -79,19 +92,23 @@ class Cocktails extends Component {
           </Col>
           <Col sm={8}>
             <CardDeck>
-              {Object.keys(cocktails).map((key, i) => (
-                <Card key={i}>
-                  <Card.Title>{Object.keys(cocktails)[i]}</Card.Title>
-                  <Card.Text>
-                    <ul>
-                      {Object.entries(cocktails[key].ingredients).map((ingredient, x) => (
-                        <li key={x}>{`${ingredient[0]}: ${ingredient[1]}`}</li>
-                      ))}
-                    </ul>
-                    {cocktails[key].instructions}
-                  </Card.Text>
-                </Card>
-              ))}
+              {Object.keys(cocktails).map((key, i) => {
+                if (this.state.selectedIngredients.filter(x => Object.keys(cocktails[key].ingredients).includes(x)).length !== 0) {
+                  return (
+                    <Card key={i}>
+                      <Card.Title>{Object.keys(cocktails)[i]}</Card.Title>
+                      <Card.Text>
+                        <ul>
+                          {Object.entries(cocktails[key].ingredients).map((ingredient, x) => (
+                            <li key={x}>{`${ingredient[0]}: ${ingredient[1]}`}</li>
+                          ))}
+                        </ul>
+                        {cocktails[key].instructions}
+                      </Card.Text>
+                    </Card>
+                  )
+                }
+              })}
             </CardDeck>
           </Col>
         </Row>
@@ -99,5 +116,20 @@ class Cocktails extends Component {
     )
   }
 }
+// <CardDeck>
+//   {Object.keys(cocktails).map((key, i) => (
+//     <Card key={i}>
+//       <Card.Title>{Object.keys(cocktails)[i]}</Card.Title>
+//       <Card.Text>
+//         <ul>
+//           {Object.entries(cocktails[key].ingredients).map((ingredient, x) => (
+//             <li key={x}>{`${ingredient[0]}: ${ingredient[1]}`}</li>
+//           ))}
+//         </ul>
+//         {cocktails[key].instructions}
+//       </Card.Text>
+//     </Card>
+//   ))}
+// </CardDeck>
 
 export default Cocktails
